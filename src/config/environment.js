bb479b2ec16acc1bf6922a8a1702d01a7925dc4a
Edmund <munjyong@gmail.com>
2020-09-18 23:30:58 +0100

    Update styles

---
+++ src/config/environment.js
@@ -0,0 +1,3 @@
+module.exports = {
+  auth: "http://localhost:3000/auth/discord/login",
+};
--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import discordLogo from "../../assets/discord-logo.svg";
+import "./index.css";
+import wave from "../../assets/wave.svg";
+import { auth } from "../../config/environment";
 
 import Button from "@material-ui/core/Button";
 import {
@@ -9,21 +13,48 @@ import {
   ThemeProvider,
   makeStyles,
 } from "@material-ui/core/styles";
-import { grey } from "@material-ui/core/colors";
+import { grey, deepPurple, indigo } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: grey[900],
+      main: indigo["A100"],
     },
   },
 });
 
 const useStyles = makeStyles({
+  base: {
+    background: "#303030",
+    height: "100vh",
+    backgroundImage: `url(${wave})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "abosolute",
+    backgroundPositionY: "bottom",
+    backgroundSize: "100%",
+  },
+  mainContent: {
+    width: "100%",
+    textAlign: "center",
+    margin: "0 auto",
+    paddingTop: "5rem",
+  },
+  title: {
+    fontSize: "64px",
+    color: "#f6f6f6",
+    marginTop: "0",
+  },
+  subtext: {
+    color: "#f6f6f6",
+    width: "50%",
+    margin: "0 auto",
+    marginBottom: "3rem",
+    fontSize: "22px",
+  },
   button: {
     width: "350px",
     height: "100px",
-    backgroundColor: "#2c2f33",
+    backgroundColor: "#7289DA",
     borderRadius: "50px",
   },
   discordLogo: {
@@ -43,39 +74,44 @@ const useStyles = makeStyles({
     display: "flex !important",
     justifyContent: "space-evenly !important",
     alignItems: "center",
+    color: "#f6f6f6",
   },
 });
 
 export function LandingPage(props) {
   const classes = useStyles();
+  const history = useHistory();
 
   const buttonContent = (
     <div className={classes.buttonContent}>
       <div className={classes.iconBackground}>
-        <img className={classes.discordLogo} src={discordLogo} />
+        <img className={classes.discordLogo} src={discordLogo} alt="discord" />
       </div>
       <p>Sign in with Discord</p>
     </div>
   );
 
   return (
-    <div>
-      <h1>AuthED</h1>
-      <article>
-        <p>
-          Authenticate yourself using your Discord account to gain access to the
-          AuthED Discord server. Once authenticated, you will receive a Verified
-          role on the server shortly.
-        </p>
-        <ThemeProvider theme={theme}>
-          <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            children={buttonContent}
-          />
-        </ThemeProvider>
-      </article>
+    <div className={classes.base}>
+      <section className={classes.mainContent}>
+        <h1 className={classes.title}>AuthED</h1>
+        <article>
+          <p className={classes.subtext}>
+            Authenticate yourself using your Discord account to gain access to
+            the AuthED Discord server. Once authenticated, you will receive a
+            Verified role on the server shortly.
+          </p>
+          <ThemeProvider theme={theme}>
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="primary"
+              children={buttonContent}
+              onClick={() => (window.location = auth)}
+            />
+          </ThemeProvider>
+        </article>
+      </section>
     </div>
   );
 }
