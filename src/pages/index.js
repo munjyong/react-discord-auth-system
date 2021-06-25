393e8198914df483f166ea107c82a33cd5275546
munjyong <munjyong@gmail.com>
2021-06-25 19:35:43 +0100

    Refactor pages structure for ES6 import/export

---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Activate } from './Activate/Activate.jsx'
+export { default as Dashboard } from './Dashboard/Dashboard.jsx'
+export { default as Landing } from './Landing/Landing.jsx'
