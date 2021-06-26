ca38ee6105fea46008427e3ac4a6b9898bca7ab0
munjyong <munjyong@gmail.com>
2021-06-26 22:21:39 +0100

    Add oauth link to button

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Landing = () => {
   return (
     <div>
-      Landing Page
+      <h1>
+        Landing Page
+      </h1>
+      <button>
+        <a href={"https://discord.com/api/oauth2/authorize?client_id=858064642227044352&redirect_uri=http%3A%2F%2Flocalhost%3A3003%2F&response_type=code&scope=identify%20email%20guilds%20guilds.join"}>
+          Discord Auth
+        </a>
+      </button>
     </div>
   );
 };
